d3d2776cfb55ce3afe0adc7c8d46f79cd2d250a9
Nathan Lesne <nathan.lesne@etu.u-bordeaux.fr>
2017-12-18 11:54:21 +0100

    Implémentation du programme de la balise

--- balise/main.cpp
+++ balise/main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <cstring>
+#include <unistd.h>
+
+#include "make-sockaddr.h"
+#include "EnvoiDonnees.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    if (argc != 7) {
+        std::cerr << "Usage: " << argv[0]
+              << " serveur port identifiant position (X,Y) cap" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+        auto adresse_serveur = remote_socket_address(argv[1], SOCK_DGRAM, argv[2]);
+          int fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+          EnvoiDonnees message (argv[3], argv[4], atoi(argv[5]));
+
+          int n = sendto(fd, &message, sizeof message,
+                         0,
+                         (sockaddr *) &adresse_serveur, sizeof adresse_serveur);
+
+          if (n == sizeof message)
+              std::cout << "Coordonnées envoyées" << std::endl;
+          else
+              std::cout << "Erreur lors de l'envoi" << std::endl;
+
+          close(fd);
+
 }
 
