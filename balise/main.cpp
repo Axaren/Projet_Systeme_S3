3cf148dfe3aea045f28df90e304550d32f17e332
Romain Roubaix <romain.roubaix@etu.u-bordeaux.fr>
2018-01-14 14:00:36 +0100

    Version 1.1 - Commentaires ok, Impossible de faire plusieurs requete :/

--- balise/main.cpp
+++ balise/main.cpp
@@ -10,25 +10,23 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     if (argc != 7) {
-        std::cerr << "Usage: " << argv[0]
-              << " serveur port identifiant X Y cap" << std::endl;
+        std::cerr << "Usage: "
+                  << "serveur port identifiant X Y cap" << std::endl;
         return EXIT_FAILURE;
     }
 
-        auto adresse_serveur = remote_socket_address(argv[1], SOCK_DGRAM, argv[2]);
-          int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    auto adresse_serveur = remote_socket_address(argv[1], SOCK_DGRAM, argv[2]);
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
 
-                             // id     , X            , Y            , cap
-          EnvoiDonnees message (argv[3], stoi(argv[4]), stoi(argv[5]), stoi(argv[6]));
-          int n = sendto(fd, &message, sizeof message,
-                         0,
-                         (sockaddr *) &adresse_serveur, sizeof adresse_serveur);
+                       // id     , X            , Y            , cap
+    EnvoiDonnees message (argv[3], stoi(argv[4]), stoi(argv[5]), stoi(argv[6]));
+    int n = sendto(fd, &message, sizeof message, 0,
+                   (sockaddr *) &adresse_serveur, sizeof adresse_serveur);
 
-          if (n == sizeof message)
-              std::cout << "Coordonnées envoyées" << std::endl;
-          else
-              std::cout << "Erreur lors de l'envoi" << std::endl;
+    if (n == sizeof message)
+        cout << "Coordonnées envoyées" << endl;
+    else
+        cerr << "ECHEC lors de l'envoi" << endl;
 
-          close(fd);
-
+    close(fd);
 }
--- consultation/main.cpp
+++ consultation/main.cpp
@@ -11,100 +11,75 @@ using namespace std;
 
 int main(int argc,  char* argv[])
 {
-    //    bind listen accept -> nouveau socket dans un int
-    //    read un 0 -> fin de la connection
-    /*
-    auto adresse_locale = local_socket_address(SOCK_DGRAM, port);
-    int fd = socket(AF_INET, SOCK_DGRAM, 0); // adresse et socket
-
-    bind(fd, (sockaddr *) & adresse_locale, sizeof(adresse_locale)); // lie le server à l'adresse locale
-
-    std::cout << "** Serveur UDP pour les balises port " << port << std::endl;
-
-    while (true) {
-
-        EnvoiDonnees m;
-        if (read(fd, &m, sizeof m) == sizeof m) {
-            if (m.id() == "123456") { // code secret pour arrêter le serveur
-                break;
-            }
-            messagesBalises.push_back(m);
-            std::cout << "Message reçu" << std::endl; // TODO ajouter le detail
-        }
-
-        // si on ecrit coords -> while cin != coords
-        // envoie la demande des coordonnées -> bind
-        // ecoute la réponse -> listen
-        // accepte la réponse -> accept
-    }
-        */
-
-
-    int sockfd, portno, n;
+    int sockfd, port, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
     char buffer[256];
 
-    if (argc < 3) {
-        fprintf(stderr,"usage %s hostname port\n", argv[0]);
-        exit(0);
+    if (argc != 3) {
+        std::cerr << "Usage: "
+                  << "serveur port" << std::endl;
+        return EXIT_FAILURE;
     }
 
-    portno = atoi(argv[2]);
+    port = atoi(argv[2]);
 
-    /* Create a socket point */
+    // Créée le socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sockfd < 0) {
-        perror("ERROR opening socket");
-        exit(1);
+        std::cerr << "ECHEC de la création du socket" << std::endl;
+        return EXIT_FAILURE;
     }
 
     server = gethostbyname(argv[1]);
 
     if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
+        std::cerr << "ECHEC ce server n'existe pas" << std::endl;
+        return EXIT_FAILURE;
     }
 
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
+    serv_addr.sin_port = htons(port);
 
-    /* Now connect to the server */
+    // Connection au serveur
     if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("ERROR connecting");
-        exit(1);
+        std::cerr << "ECHEC impossible de se connecter" << std::endl;
+        return EXIT_FAILURE;
     }
 
-    /* Now ask for a message from the user, this message
-     * will be read by server
-    */
+    while(true) {
 
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
+        // Demande à l'utilisateur d'entrer une commande
+        // qui sera lue par le serveur
 
-    /* Send message to the server */
-    n = write(sockfd, buffer, strlen(buffer));
+        cout << "Veuillez entrer une commande: " << endl;
+        bzero(buffer,256); // vide le buffer
+        fgets(buffer,255,stdin);
 
-    if (n < 0) {
-        perror("ERROR writing to socket");
-        exit(1);
+        // Envoi du message au server
+        n = write(sockfd, buffer, strlen(buffer));
+
+        if (n < 0) {
+            cerr << "ECHEC lors de l'ecriture au socket" << endl;
+            return EXIT_FAILURE;
+        }
+
+        // Lecture de la réponse du serveur
+        bzero(buffer,256);
+        n = read(sockfd, buffer, 255);
+
+        if (n < 0) {
+            cerr << "ECHEC lecture du socket" << endl;
+            return EXIT_FAILURE;
+        }
+
+        printf("%s\n",buffer);
     }
 
-    /* Now read server response */
-    bzero(buffer,256);
-    n = read(sockfd, buffer, 255);
-
-    if (n < 0) {
-        perror("ERROR reading from socket");
-        exit(1);
-    }
-
-    printf("%s\n",buffer);
-    return 0;
+    return EXIT_SUCCESS;
 }
 
